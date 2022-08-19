f04e7ab2fe799f34af57a2d0a176e52f582bc1f6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2022-08-18 19:19:35 -0500

    commit tree d7f51a73435a7bdfdc35dc983869d0e235db4e25

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() {
+    return ["Antonia", "Nuru"];
+}
+
+const returnLastTwoDrivers = function() {
+    return ["Amari", "Mo"];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+    return function (fare) {
+        return fare * integer;
+    };
+};
+
+function fareDoubler(fare) {
+    const cfm = createFareMultiplier()
+    cfm();
+    return fare * 2;
+}
+
+function fareTripler(fare) {
+    const cfm = createFareMultiplier()
+    cfm();
+    return fare * 3;  
+}
+
+function selectDifferentDrivers(selectingDrivers, func) {
+    if (func === returnFirstTwoDrivers) {
+        return ["Antonia", "Nuru"];
+    } else if(func === returnLastTwoDrivers) {
+        return ["Amari", "Mo"];
+    };
+}
